af9a575e27598fafb553b1639c5a39dd3fe3bcb7
VadimPitsOfficial <50966751+VadimPitsOfficial@users.noreply.github.com>
2019-09-28 15:44:09 +0300

    Update batch.py

--- Laboratory1/batch.py
+++ Laboratory1/batch.py
@@ -8,9 +8,12 @@ BEGIN BATCH
 INSERT INTO vadim_pits."Products_Characteristic_Store_Recommend"(id_prod, name_prod, colour, price, counter, id_store, name_store, result, final_name_1)
 VALUES (1,{"name": 'Lexus', "model": 'LX350'}, 'white', 30000, 2, 1, 'Japan', {'is available':{'Japan'}}, {"name": 'Lexus', "model": 'LX350'});
 
-DELETE counter
-FROM vadim_pits."Products_Characteristic_Store_Recommend"
-WHERE id_prod = 1 AND colour = 'white' AND id_store = 1;
+UPDATE vadim_pits."Products_Characteristic_Store_Recommend"
+SET
+  price= 37000,
+  name_store = 'USA'
+WHERE id_prod = 1 AND colour = 'white' AND id_store = 1 IF EXISTS;
+
 
 APPLY BATCH;
 """
